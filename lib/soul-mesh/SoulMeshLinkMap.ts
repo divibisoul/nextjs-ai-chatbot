104c7eee2f04d083759f207e2045a43cdc16830a
divibisoul <divibi2011@gmail.com>
2026-08-26 02:07:57 -0300

    mesh: canonicalize N04 link map

--- lib/soul-mesh/SoulMeshLinkMap.ts
+++ lib/soul-mesh/SoulMeshLinkMap.ts
@@ -1,3 +1,3 @@
-import type { SoulMeshPeer } from './SoulMeshPeerMatrix';
-export type SoulMeshLink = { local: 'chatbot'; peer: SoulMeshPeer; in: boolean; out: boolean };
-export const R4_LINKS: SoulMeshLink[] = ['aeternum','nexus','eternium','chatbots','chatbot-2000'].map((peer) => ({ local: 'chatbot', peer: peer as SoulMeshPeer, in: true, out: true }));
+import { SOUL_MESH_PEERS, type SoulMeshPeer } from './SoulMeshPeerMatrix';
+export type SoulMeshLink = { local: 'N04'; peer: SoulMeshPeer; in: boolean; out: boolean };
+export const R5_LINKS: SoulMeshLink[] = SOUL_MESH_PEERS.map((peer) => ({ local: 'N04', peer, in: true, out: true }));
