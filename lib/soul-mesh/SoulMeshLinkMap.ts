5cd68186343c0ff6699373b1da288a6704f71dcb
divibisoul <divibi2011@gmail.com>
2026-09-25 00:31:42 -0300

    fix(N05): align legacy Mesh matrices and ping topology with N07

--- lib/soul-mesh/SoulMeshLinkMap.ts
+++ lib/soul-mesh/SoulMeshLinkMap.ts
@@ -1,3 +1,3 @@
 import type { SoulMeshPeer } from './SoulMeshPeerMatrix';
 export type SoulMeshLink = { local: 'N05'; peer: SoulMeshPeer; in: boolean; out: boolean };
-export const R4_LINKS: SoulMeshLink[] = ['N01','N02','N03','N04','N06'].map((peer) => ({ local: 'N05', peer: peer as SoulMeshPeer, in: true, out: true }));
+export const R4_LINKS: SoulMeshLink[] = ['N01','N02','N03','N04','N06','N07'].map((peer) => ({ local: 'N05', peer: peer as SoulMeshPeer, in: true, out: true }));
